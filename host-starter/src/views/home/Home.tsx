046a4be3d868908424389f5183f56e763da44af8
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-09-19 14:54:51 +0300

    space

--- host-starter/src/views/home/Home.tsx
+++ host-starter/src/views/home/Home.tsx
@@ -1,12 +1,12 @@
 // Import the Front component from the front_and_sidebar mfe
-import Front from 'front_and_sidebar/Front'
+import Front from "front_and_sidebar/Front";
 // Import the useMediaContext hook from the mediastore mfe
-import {useMediaContext} from 'mediastore/ContextHooks'
-import {MediaContextType}  from '@sharedTypes/DBTypes';
+import { useMediaContext } from "mediastore/ContextHooks";
+import { MediaContextType } from "@sharedTypes/DBTypes";
 
 const Home = () => {
   // Use the useMediaContext hook to get the mediaItems
-  const { mediaItems} = useMediaContext() as MediaContextType;
+  const { mediaItems } = useMediaContext() as MediaContextType;
   // Pass the mediaItems to the Front component
   return <div>{mediaItems && <Front mediaItems={mediaItems} />}</div>;
 };
